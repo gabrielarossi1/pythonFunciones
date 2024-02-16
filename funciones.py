c0265f7f36e5c240e105193e21cf17599973f898
Gabriela Rossi <gabi_20_14@hotmail.com>
2024-02-15 21:01:19 -0300

    acrividad

--- funciones.py
+++ funciones.py
@@ -1,9 +1,14 @@
+# Crear un pequeño programa que realice una función aritmética que permita al usuario ingresar datos por la terminal acorde a distintas opciones.  Para ellos debemos definir una función que reciba parámetros:
+# Combinar funciones nativas y funciones definidas,
+# condicionales, y bucles.
+# Si el usuario ingresa el nro 1, realiza la acción A.
+# Si el usuario ingresa el nro 2, realiza la acción B.
+
 def accion_A(num1, num2):
     return num1 + num2
 
 def accion_B(num1, num2):
     return num1 - num2
-
 
 def main():
     opcion = input("Ingrese 1 para realizar la acción A o 2 para realizar la acción B: ")
